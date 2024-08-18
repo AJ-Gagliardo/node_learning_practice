88920bcab4c31ef2ea69324dbe5452f061f3022a
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-08-17 23:08:21 -0700

    getting out fo callback hell on node with .then and .catch

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -6,12 +6,14 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
+    .then((res) => {
       console.log(res.body);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) return console.log(err.message);
         console.log('random dog image saved to file');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
