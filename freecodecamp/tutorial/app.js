8e0c8595127dfdccde20493748d71e620ad5399e
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-08-31 23:58:44 -0700

    making changes on the node files

--- freecodecamp/tutorial/app.js
+++ freecodecamp/tutorial/app.js
@@ -13,10 +13,24 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everthing default)
 
-const _ = require("lodash");
+// const _ = require("lodash");
 
-const items = [1, [2, 3, [4]]];
+// const items = [1, [2, 3, [4]]];
 
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log("hello people");
+// const newItems = _.flattenDeep(items);
+// console.log(newItems);
+// console.log("hello people");
+
+const { readFile } = require("fs");
+
+//check file path!!
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if(err{
+    console.log(err)
+    return
+  }
+  console.log(result)
+  console.log('completed first task')
+  )
+});
+console.log('starting enxt tast')
