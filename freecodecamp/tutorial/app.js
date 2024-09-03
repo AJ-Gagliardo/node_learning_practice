6227cc444577c3296e6ed12f1ce0ff22b917222b
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-09-03 12:05:54 -0700

    learning and practicing async patterns but in node native option

--- freecodecamp/tutorial/app.js
+++ freecodecamp/tutorial/app.js
@@ -1,21 +1,39 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile("./content/first.txt", "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+// an also use it like this
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-getText("./content/first.txt")
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+// this is without node options to do promises
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
 
 const start = async () => {
-  const first = await getText("./content/first.txt");
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `this is awesome: ${first}, ${second}`,
+      { flag: "a" }
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+start();
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
