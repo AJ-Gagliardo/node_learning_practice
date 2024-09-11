9f767753a64beb877019f596e7c47db993539012
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-09-11 13:41:33 -0700

    testing different routes with the http

--- freecodecamp/02-express-tutorial/app.js
+++ freecodecamp/02-express-tutorial/app.js
@@ -1,10 +1,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Home page</h1>");
-  res.end();
+  // console.log("user hit the server");
+  // console.log(req.method);
+
+  console.log(req.url);
+
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Home page</h1>");
+    res.end();
+  }
+  //about page
+  else if (url === "/about") {
+  }
+
+  // 404
 });
 
 server.listen(5000);
