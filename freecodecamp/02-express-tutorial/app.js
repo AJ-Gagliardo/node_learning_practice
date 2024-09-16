6b648fb99477d51b063e7c795fe51a7240caa09b
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-09-15 22:26:24 -0700

    using a data.js as the api

--- freecodecamp/02-express-tutorial/app.js
+++ freecodecamp/02-express-tutorial/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const { products } = require("./data");
+const { people } = require("./data");
 
 app.get("/", (req, res) => {
-  res.json([{ name: "john" }, { name: "susan" }]);
+  res.json(products);
 });
 
 app.listen(5000, () => {
