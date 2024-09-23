32e637f885294a75a229fc5044bc5649aadafe7c
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-09-22 19:03:27 -0700

    learning how to connect mongoose, and mongo db to my project

--- freecodecamp/03-task-manager/starter/app.js
+++ freecodecamp/03-task-manager/starter/app.js
@@ -1,10 +1,16 @@
+require("./db/connect");
 const express = require("express");
 const app = express();
-
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 // routes
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
+
+// middleware
+app.use(express.json());
 
 //app.get('/api/v1/tasks)        // -- get all the tasks
 //app.post('/api/v1/tasks)       // -- create a new task
@@ -12,5 +18,17 @@ app.get("/hello", (req, res) => {
 //app.patch('/api/v1/tasks/:id') // --update single task
 //app.delete('/api/v1tasks/:id) // -- delete single task
 
+// get all the tasks
+app.use("/api/v1/tasks", tasks);
+
 const port = 3000;
-app.listen(port, console.log(`Server is listening on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ freecodecamp/03-task-manager/starter/db/connect.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
+
+/*
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to the DB"))
+  .catch((err) => console.log(err));
+*/
