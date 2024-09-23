4db1557854ed5b51d07f793512bea4cd64ece7d8
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-09-23 01:00:34 -0700

    learning how to use .find() to get all the tasks

--- freecodecamp/03-task-manager/starter/models/Task.js
+++ freecodecamp/03-task-manager/starter/models/Task.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide a name"],
+    trim: true, //will trim away empty spaces
+    maxlength: [20, "name can not be more than 20"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
+
+//simple schema
+//  name: String,
+// completed: Boolean,
 
 module.exports = mongoose.model("Task", TaskSchema);
