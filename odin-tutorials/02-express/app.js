939430d629056d5188a4f2a4daa6580656a1b0ff
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-09-26 13:28:23 -0700

    just going back to res.send and req.params and req.queries to rpactice what i learned this week

--- odin-tutorials/02-express/app.js
+++ odin-tutorials/02-express/app.js
@@ -4,6 +4,25 @@ require("dotenv").config();
 
 app.get("/", (req, res) => res.send("hello, world!"));
 
+app.get("/user/:id", function (req, res) {
+  res.send("user " + req.params.id);
+});
+app.post("/messages", (req, res) =>
+  res.send("This is where you can see any messages.")
+);
+
+app.get("/:username/messages", (req, res) => {
+  console.log("Params: ", req.params);
+  console.log("Query", req.query);
+  res.end();
+});
+
+app.get("/:username/messages/:messageId", (req, res) => {
+  console.log(req.params);
+
+  res.end();
+});
+
 const PORT = process.env.PORT;
 // const PORT = 3000;
 
